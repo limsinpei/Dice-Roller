c2585a574dc7c14b376697fc426e42c6c9b481ac
anreh <anurehka@gmail.com>
2019-10-23 12:21:03 +0800

    practical1

---
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.practical1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.Format
+import java.util.*
+
+//import android.widget.Toast
+//import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+    }
+
+
+    private fun rollDice()
+    {
+        val num = 0
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+
+    }
+
+    private fun getRandomDiceImage() : Int{
+        val randomInt = Random().nextInt(6) + 1
+
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //resultText.text = randomInt.toString()
+        //val diceImage: ImageView = findViewById(R.id.dice_image)
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+
+    }
+}
